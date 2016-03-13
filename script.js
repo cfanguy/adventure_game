255a773a3e52f54079478a4c1cfbe288b29e6332
cfanguy <cfanguy@gmail.com>
2016-03-12 18:56:22 -0600

    Changed snake movement, made playable area bigger, and snakes are now added at set intervals.

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function startGame() {
 	snakes = [];
 	score = 0;
 	player = null;
-	player = rect(290, 290, 26, 34);
+	player = rect(390, 390, 26, 34);
     sword = rect(-30, -30, 20, 11);
 	player.velocity = { x: 0, y: 0 };
 	gameOver = false;
@@ -39,16 +39,16 @@ function startGame() {
 	createSnakes();
 	
 	// horizontal blocks
-	for(var i = 0; i < 620; i+=20) {
+	for(var i = 0; i < 800; i+=20) {
 		rects.push(rect(i, 0, 20, 20));
-		rects.push(rect(i, 580, 20, 20));
+		rects.push(rect(i, 780, 20, 20));
 	}
 
 
 	// vertical blocks
-	for(var i = 0; i < 620; i+=20) {
+	for(var i = 0; i < 800; i+=20) {
 		rects.push(rect(0, i, 20, 20));
-		rects.push(rect(580, i, 20, 20));
+		rects.push(rect(780, i, 20, 20));
 	}
 }
 
@@ -59,31 +59,31 @@ function rect(x, y, w, h) {
 
 
 function createSnakes() {
-	snakes.push(snakeEnemy(60, 60, 25, 25));
+	snakes.push(snakeEnemy(160, 160, 25, 25));
 	snakes[0].velocity = { x: 0, y: 0 };
-	snakes.push(snakeEnemy(500, 60, 25, 25));
+	snakes.push(snakeEnemy(600, 160, 25, 25));
 	snakes[1].velocity = { x: 0, y: 0 };
-	snakes.push(snakeEnemy(60, 500, 25, 25));
+	snakes.push(snakeEnemy(160, 600, 25, 25));
 	snakes[2].velocity = { x: 0, y: 0 };
-	snakes.push(snakeEnemy(500, 500, 25, 25));
+	snakes.push(snakeEnemy(600, 600, 25, 25));
 	snakes[3].velocity = { x: 0, y: 0 };
 	
-	snakes.push(snakeEnemy(100, 100, 25, 25));
+	snakes.push(snakeEnemy(200, 200, 25, 25));
 	snakes[4].velocity = { x: 0, y: 0 };
-	snakes.push(snakeEnemy(460, 100, 25, 25));
+	snakes.push(snakeEnemy(560, 200, 25, 25));
 	snakes[5].velocity = { x: 0, y: 0 };
-	snakes.push(snakeEnemy(100, 460, 25, 25));
+	snakes.push(snakeEnemy(200, 560, 25, 25));
 	snakes[6].velocity = { x: 0, y: 0 };
-	snakes.push(snakeEnemy(460, 460, 25, 25));
+	snakes.push(snakeEnemy(560, 560, 25, 25));
 	snakes[7].velocity = { x: 0, y: 0 };
 	
-	snakes.push(snakeEnemy(80, 80, 25, 25));
+	snakes.push(snakeEnemy(180, 180, 25, 25));
 	snakes[8].velocity = { x: 0, y: 0 };
-	snakes.push(snakeEnemy(480, 80, 25, 25));
+	snakes.push(snakeEnemy(580, 180, 25, 25));
 	snakes[9].velocity = { x: 0, y: 0 };
-	snakes.push(snakeEnemy(80, 480, 25, 25));
+	snakes.push(snakeEnemy(180, 580, 25, 25));
 	snakes[10].velocity = { x: 0, y: 0 };
-	snakes.push(snakeEnemy(480, 480, 25, 25));
+	snakes.push(snakeEnemy(580, 580, 25, 25));
 	snakes[11].velocity = { x: 0, y: 0 };
 }
 
@@ -171,14 +171,23 @@ function update(frameNum) {
 	movePlayer(player, player.velocity.x, player.velocity.y);
 	
 	for(var i = 0; i < snakes.length; i++) {
-		// set snakes to go random places
-        if(frameNum % 40 == 0) {
-            snakes[i].velocity.x = Math.round(Math.random()) == 1 ? 1 : -1;
-            snakes[i].velocity.y = Math.round(Math.random()) == 1 ? 1 : -1;
+        if(frameNum === 1 || Math.round(Math.random()) === 1) {
+            // set snakes to go random places
+            if(frameNum % 40 === 0 || frameNum === 1) {
+                snakes[i].velocity.x = Math.floor(Math.random() * 3 - 1);
+                snakes[i].velocity.y = Math.floor(Math.random() * 3 - 1);
+            }
         }
-        
         moveSnake(snakes[i], snakes[i].velocity.x, snakes[i].velocity.y, i);
-	}
+    }
+    
+    if(frameNum % 120 === 0 && frameNum !== 0) {
+        var newSnakeX = Math.round(Math.random()) === 1 ? 700 : 260;
+        var newSnakeY = Math.round(Math.random()) === 1 ? 700 : 260;
+        
+        snakes.push(snakeEnemy(newSnakeX, newSnakeY, 25, 25));
+        snakes[snakes.length - 1].velocity = { x: 0, y: 0 };
+    }
 	
 	document.getElementById("score").innerHTML = score.toString();
 }
@@ -209,7 +218,7 @@ function draw() {
         c.drawImage(dImg, player.x - 6, player.y - 5);
 	}
     
-    // draw sword on space
+    // draw sword on space key press
     var swRimg = document.getElementById('sword_r');
     var swLimg = document.getElementById('sword_l');
     if(keyPressed[32] === true) {
